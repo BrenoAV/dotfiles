319aa5865cebfbb84803aaf7324a5bd3a05a20dd
brenoAV <brenoalencarvieira07@gmail.com>
2024-09-29 08:33:09 -0300

    removing catpuccin

--- neovim-lua-lang/lua/core/lazy.lua
+++ neovim-lua-lang/lua/core/lazy.lua
@@ -97,7 +97,6 @@ require("lazy").setup({
         },
         { "kkoomen/vim-doge" },
         -- Colorscheme
-        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
         { "Mofiqul/vscode.nvim", name = "vscode", priority = 1000 }
     },
 })
