d9e7edd13c2d46eb09b87b4b07b92b0904f2f16d
brenoAV <brenoalencarvieira07@gmail.com>
2024-10-16 07:16:40 -0300

    removing lua lang configuration of neovim

--- nvim/lua/plugins_config/lsp-config.lua
+++ nvim/lua/plugins_config/lsp-config.lua
@@ -4,6 +4,13 @@ require("mason-lspconfig").setup({
 }
 )
 
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
 -- LSP configuration
 
 local on_attach = function(client, bufnr)
