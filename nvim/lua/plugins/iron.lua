7237982cc66bca7b20841a70e2cb8ea62eb8cc68
brenoAV <brenoalencarvieira07@gmail.com>
2025-05-10 10:50:07 -0300

    removing vim-slime.lua file

--- nvim/lua/plugins/iron.lua
+++ nvim/lua/plugins/iron.lua
@@ -9,32 +9,11 @@ iron.setup {
     -- Your repl definitions come here
     repl_definition = {
       python = {
-        command = { "ipython", "--no-autoindent" },  -- or { "ipython", "--no-autoindent" }
+        command = { "python" },
         format = common.bracketed_paste_python,
         block_dividers = { "# %%", "#%%" },
       }
     },
-    -- set the file type of the newly created repl to ft
-    -- bufnr is the buffer id of the REPL and ft is the filetype of the 
-    -- language being used for the REPL. 
-    repl_filetype = function(bufnr, ft)
-      return ft
-      -- or return a string name such as the following
-      -- return "iron"
-    end,
-    -- How the repl window will be displayed
-    -- See below for more information
-    -- repl_open_cmd = view.bottom(20),
-
-    -- repl_open_cmd can also be an array-style table so that multiple 
-    -- repl_open_commands can be given.
-    -- When repl_open_cmd is given as a table, the first command given will
-    -- be the command that `IronRepl` initially toggles.
-    -- Moreover, when repl_open_cmd is a table, each key will automatically
-    -- be available as a keymap (see `keymaps` below) with the names 
-    -- toggle_repl_with_cmd_1, ..., toggle_repl_with_cmd_k
-    -- For example,
-    -- 
     repl_open_cmd = {
       view.split.vertical.rightbelow("%40"), -- cmd_1: open a repl to the right
       view.split.rightbelow("%25")  -- cmd_2: open a repl below
