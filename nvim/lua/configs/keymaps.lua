1bc1fe065a562fda3704582cc87bb23852f70627
brenoAV <brenoalencarvieira07@gmail.com>
2025-06-15 10:01:40 -0300

    structuring better the files

---
+++ nvim/lua/configs/keymaps.lua
@@ -0,0 +1,37 @@
+-- Moving around, tabs, windows and buffers
+
+-- Create Splits
+vim.api.nvim_set_keymap('n', 'th', ':split<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tv', ':vsplit<CR>', { noremap = true })
+-- Create a tab
+vim.api.nvim_set_keymap('n', 'tn', ':tabe<CR>', { noremap = true })
+-- Navigating between tabs
+vim.api.nvim_set_keymap('n', '<leader>1', '<CMD>tabn 1<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>2', '<CMD>tabn 2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>3', '<CMD>tabn 3<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>4', '<CMD>tabn 4<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>5', '<CMD>tabn 5<CR>', { noremap = true, silent = true })
+
+-- navigate between Buffers
+vim.api.nvim_set_keymap('n', '<leader>n', ':bn<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', ':bp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d', ':bd<CR>', { noremap = true, silent = true})
+
+-- delete a buffer without losing the split window
+vim.api.nvim_set_keymap('n', '<C-x>', ':bp\\|bd #<CR>', { noremap = true })
+
+-- Move lines up and down
+vim.api.nvim_set_keymap('n', '<M-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<M-Down>', '<Esc>:m .+1<CR>==gi', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<M-Up>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<M-Down>', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<M-Up>', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
+
+-- Move between wrapped text
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-n>', ':noh<CR>', { noremap = true, silent = true })
+
+-- Terminal
+vim.keymap.set('t', '<leader><Esc>', [[<C-\><C-n>]], { noremap = true })
--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -1,4 +1,3 @@
-vim.opt.mouse = 'a'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4               -- show existing tab with 4 spaces width
@@ -27,41 +26,3 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 vim.g.python3_host_prog = '/usr/bin/python3'
-
--- Moving around, tabs, windows and buffers
-
--- Create Splits
-vim.api.nvim_set_keymap('n', 'th', ':split<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tv', ':vsplit<CR>', { noremap = true })
--- Create a tab
-vim.api.nvim_set_keymap('n', 'tn', ':tabe<CR>', { noremap = true })
--- Navigating between tabs
-vim.api.nvim_set_keymap('n', '<leader>1', '<CMD>tabn 1<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>2', '<CMD>tabn 2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>3', '<CMD>tabn 3<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>4', '<CMD>tabn 4<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>5', '<CMD>tabn 5<CR>', { noremap = true, silent = true })
-
--- navigate between Buffers
-vim.api.nvim_set_keymap('n', '<leader>n', ':bn<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>p', ':bp<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>d', ':bd<CR>', { noremap = true, silent = true})
-
--- delete a buffer without losing the split window
-vim.api.nvim_set_keymap('n', '<C-x>', ':bp\\|bd #<CR>', { noremap = true })
-
--- Move lines up and down
-vim.api.nvim_set_keymap('n', '<M-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<M-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<M-Down>', '<Esc>:m .+1<CR>==gi', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<M-Up>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<M-Down>', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<M-Up>', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
-
--- Move between wrapped text
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-n>', ':noh<CR>', { noremap = true, silent = true })
-
--- Terminal
-vim.keymap.set('t', '<leader><Esc>', [[<C-\><C-n>]], { noremap = true })
